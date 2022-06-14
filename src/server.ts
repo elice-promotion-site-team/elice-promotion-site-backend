7abd1363cfc154bc311880c0af2296e6f6a5ed2a
chss3339 <chss3339@gmail.com>
2022-06-14 21:28:00 +0900

    nodemon 적용, socket.io 설치

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import { Server } from 'socket.io';
+import http from 'http';
 
 const app = express();
 app.use(cors());
@@ -11,4 +13,10 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('test');
 });
 
-app.listen(PORT);
+const server = app.listen(PORT, () => console.log(`server is running ${PORT}`));
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('socket 실행');
+});
