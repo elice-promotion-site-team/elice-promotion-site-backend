959c13ee5deadbb2210fe5c6e299a0b5ce051763
chss3339 <chss3339@gmail.com>
2022-06-18 05:09:11 +0900

    추가 - 현재 접속자 목록

--- src/socket.ts
+++ src/socket.ts
@@ -3,21 +3,24 @@ import { Server } from 'socket.io';
 function webSocket(server: any) {
   const io = new Server(server);
 
+  const clientsList: any = {};
+
   // socket 연결중
   io.sockets.on('connection', (socket: any) => {
     console.log('socket 접속');
 
     // 새로운 접속자 발생
     socket.on('newUser', (newUser: any) => {
+      // 접속자의 name 속성에 이름 저장
+      (socket as any).name = newUser;
       // 현재 접속자 수
       const clientsCount = io.sockets.server.engine.clientsCount;
       console.log(clientsCount);
-      // 현재 접속자 목록
-      const clientsList: any = {};
+      // 현재 접속자 목록에 새로운 접속자 추가
       io.sockets.sockets.forEach((data: any) => {
         clientsList[data.name] = data.name;
-        console.log(data.name);
       });
+
       console.log(clientsList);
 
       // 새로운 접속자 나에게 알림
@@ -33,9 +36,6 @@ function webSocket(server: any) {
         clientsCount,
         clientsList,
       });
-
-      // 접속자의 name 속성에 이름 저장
-      (socket as any).name = newUser;
     });
 
     // 메세지 전송 발생
@@ -55,11 +55,8 @@ function webSocket(server: any) {
       const clientsCount = io.sockets.server.engine.clientsCount;
       console.log(clientsCount);
 
-      // 현재 접속자 이름
-      const clientsList: any = {};
-      io.sockets.sockets.forEach((data: any) => {
-        clientsList[data.name] = data.name;
-      });
+      // 채팅 종료한 사람은 접속자 목록에서 삭제
+      delete clientsList[socket.name];
       console.log(clientsList);
 
       // 채팅 종료한 접속자를 다른사람들에게 알림
