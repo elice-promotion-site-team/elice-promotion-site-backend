266dac85e5395da7694f248eb632721fa6e39658
chss3339 <chss3339@gmail.com>
2022-06-18 23:51:18 +0900

    추가 - 메세지 시간 나오게

--- src/socket.ts
+++ src/socket.ts
@@ -15,19 +15,24 @@ function webSocket(server: any) {
       (socket as any).name = newUser;
       // 현재 접속자 수
       const clientsCount = io.sockets.server.engine.clientsCount;
-      console.log(clientsCount);
       // 현재 접속자 목록에 새로운 접속자 추가
       io.sockets.sockets.forEach((data: any) => {
         clientsList[data.name] = data.name;
       });
 
-      console.log(clientsList);
+      // 현재 시간
+      let today = new Date(); // 현재 시간
+      let hours = today.getHours(); // 시
+      let minutes = today.getMinutes(); // 분
+      let now = `${hours}:${minutes}`;
 
+      console.log(clientsCount, clientsList, now);
       // 새로운 접속자 나에게 알림
       socket.emit('update', {
         msg: `${newUser}(나)님이 접속했습니다.`,
         clientsCount,
         clientsList,
+        time: now,
       });
 
       // 새로운 접속자 다른사람들에게 알림
@@ -35,18 +40,32 @@ function webSocket(server: any) {
         msg: `${newUser}님이 접속했습니다.`,
         clientsCount,
         clientsList,
+        now,
       });
     });
 
     // 메세지 전송 발생
-    socket.on('chat message', (msg: any) => {
+    socket.on('chat message', (data: any) => {
       // 메세지 전송자 이름 추출
       const name = (socket as any).name;
-      console.log(msg);
+      console.log(data);
+
+      // 현재 시간
+      let today = new Date(); // 현재 시간
+      let hours = today.getHours(); // 시
+      let minutes = today.getMinutes(); // 분
+      let now = `${hours}:${minutes}`;
+
       // 다른사람 화면에 보여지는 메세지
-      socket.broadcast.emit('chat message', `${msg.name}: ${msg.msg}`);
+      socket.broadcast.emit('chat message', {
+        msg: `${data.name}: ${data.msg}`,
+        time: now,
+      });
       // 내 화면에 보여지는 메세지
-      socket.emit('chat message', `나: ${msg.msg}`);
+      socket.emit('chat message', {
+        msg: `나: ${data.msg}`,
+        time: now,
+      });
     });
 
     // 접속자 채팅 종료
@@ -59,11 +78,18 @@ function webSocket(server: any) {
       delete clientsList[socket.name];
       console.log(clientsList);
 
+      // 현재 시간
+      let today = new Date(); // 현재 시간
+      let hours = today.getHours(); // 시
+      let minutes = today.getMinutes(); // 분
+      let now = `${hours}:${minutes}`;
+
       // 채팅 종료한 접속자를 다른사람들에게 알림
       socket.broadcast.emit('update', {
         msg: `${(socket as any).name}님이 나가셨습니다.`,
         clientsCount,
         clientsList,
+        time: now,
       });
     });
   });
