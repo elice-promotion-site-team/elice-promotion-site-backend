23834a4681e17972eb1680b49fdfcaca6ec5ee3d
chss3339 <chss3339@gmail.com>
2022-06-20 21:40:23 +0900

    추가 - dev 코드 복사

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,4 +10,5 @@ const db = mongoose.connection;
 db.on('connected', () => console.log('정상적으로 MongoDB 서버에 연결되었습니다.  ' + DB_URL));
 db.on('error', (error) => console.error('\nMongoDB 연결에 실패하였습니다...\n' + DB_URL + '\n' + error));
 
+export * from './models/guestbook-model';
 export * from './models/chat-model';
